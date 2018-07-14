feb8d4fd24659f38d5f1c8e9ad5fc6562397480f
Regina Johnson <ReginaBFly@yahoo.com>
2018-07-13 20:23:17 -0400

    refactor: Change service worker to render website offline

--- sw.js
+++ sw.js
@@ -1,49 +1,60 @@
-self.addEventListener('install', function (event) {
-    // Perform install steps
-  }); 
-  
-  let CACHE_NAME = 'restaurant-cache';
-  let urlsToCache = [
-    '/',
-    './index.html',
-    './restaurant.html',
-    './css/styles.css',
-    './js/dbhelper.js',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './data/restaurants.json',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg',
-  ];
-  
-  self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function (cache) {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
-  
-  self.addEventListener('activate',  event => {
-    event.waitUntil(self.clients.claim());
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request, {ignoreSearch:true}).then(response => {
-        return response || fetch(event.request);
+const staticCacheName = 'restaurant-static-v';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          './index.html',
+          './restaurant.html',
+          './css/styles.css',
+          './js/dbhelper.js',
+          './js/main.js',
+          './js/restaurant_info.js',
+          './data/restaurants.json',
+          './img/1.jpg',
+          './img/2.jpg',
+          './img/3.jpg',
+          './img/4.jpg',
+          './img/5.jpg',
+          './img/6.jpg',
+          './img/7.jpg',
+          './img/8.jpg',
+          './img/9.jpg',
+          './img/10.jpg',
+        ])
+        .catch(error => {
+          console.log('Caches open failed: ' + error);
+        });
       })
-      .catch(err => console.log(err, event.request))
-    );
-  });
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('restaurant-static-') && cacheName !== staticCacheName;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(staticCacheName).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
+  );
+});
